323415ee361555c25a1640bad55da3a4ed922c77
JerYoMat <jeremy.emata@gmail.com>
2019-02-03 17:19:03 -0500

    WIP

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,13 +2,25 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @user1 = users(:test_user_michael)
+    @user2 = users(:test_user_angelo)
   end
 
+  test "should be valid" do 
+    assert @user1.valid?
+  end 
+
+
   test "should get index" do
-    get users_url
-    assert_response :success
+    log_in_as(@user1)
+    if logged_in?
+      get users_url
+      assert_response :success
+    else  
+      assert_redirected_to root_path 
+    end 
   end
+
 
   test "should get new" do
     get new_user_url
@@ -24,23 +36,23 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show user" do
-    get user_url(@user)
+    get user_url(@user1)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_user_url(@user)
+    get edit_user_url(@user1)
     assert_response :success
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: {  } }
-    assert_redirected_to user_url(@user)
+    patch user_url(@user1), params: { user: {  } }
+    assert_redirected_to user_url(@user1)
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
-      delete user_url(@user)
+      delete user_url(@user1)
     end
 
     assert_redirected_to users_url
