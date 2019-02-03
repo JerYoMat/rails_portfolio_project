24d9cfda91eb636b508dda1906a069d1b96fb497
JerYoMat <jeremy.emata@gmail.com>
2019-02-03 16:33:31 -0500

    fixed typo

--- db/migrate/20190203193744_create_learn_tips.rb
+++ db/migrate/20190203193744_create_learn_tips.rb
@@ -1,9 +1,9 @@
 class CreateLearnTips < ActiveRecord::Migration[5.2]
   def change
     create_table :learn_tips do |t|
-      t.string :name, 
-      t.string :link, 
-      t.text :description,
+      t.string :name 
+      t.string :link 
+      t.text :description
       t.belongs_to :user 
       t.belongs_to :lesson
       t.timestamps
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,32 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  def log_in(user)
+    session[:user_id] = user.id
+  end 
+  
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])  # Same as @current_user = @current_user || User.find_by(id: session[:user_id])
+    end 
+  end
 
+  def logged_in?
+     !current_user.nil?
+  end 
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password')
+    post login_path, params: { session: { email: user.email,
+                                          password: password } }
+  end
 end
