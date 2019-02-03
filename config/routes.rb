0b1660f96df8fb3d2d7f36bfd3c00d4d7337c130
JerYoMat <jeremy.emata@gmail.com>
2019-02-03 16:39:29 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users 
+  resources :learn_tips 
+  resources :lessons, only: [:index, :show] do 
+    resources :learn_tips, only: [:index, :new]
+  end 
 end
