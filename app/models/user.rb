2491a108591c91ad527ca88fbe94fdf2e6bd5e83
JerYoMat <jeremy.emata@gmail.com>
2019-02-03 17:11:50 -0500

    added digest method to generate password_digest for fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :lessons, through: :learn_tips
 
   has_secure_password 
+
+  def User.digest(string)  #needed to set up fixtures/users.yml Uses low 
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
