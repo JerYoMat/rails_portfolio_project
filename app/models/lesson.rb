4ea68523aba38d14bdc1b831df8bc3a142c9467f
JerYoMat <jeremy.emata@gmail.com>
2019-02-03 16:30:12 -0500

    added associations and secure password

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,2 +1,4 @@
 class Lesson < ApplicationRecord
+    has_many :learn_tips
+    has_many :users, through: learn_tips
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :learn_tips 
+  has_many :lessons, through: :learn_tips
+
+  has_secure_password 
 end
