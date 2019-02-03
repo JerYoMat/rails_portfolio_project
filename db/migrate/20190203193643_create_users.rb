2f4c31d65515799b8e7938d75e6e3aac6cb5bfe4
JerYoMat <jeremy.emata@gmail.com>
2019-02-03 16:29:55 -0500

    added migrations

--- db/migrate/20190203193643_create_users.rb
+++ db/migrate/20190203193643_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :password_digest 
+      t.string :email 
+      t.boolean :has_graduated
       t.timestamps
     end
   end
