40970eab4da61d91b0792bec780629ddda75dbb5
JerYoMat <jeremy.emata@gmail.com>
2019-02-03 22:01:33 -0500

    modified setup resulting in test now running.

--- test/models/learn_tip_test.rb
+++ test/models/learn_tip_test.rb
@@ -1,18 +1,21 @@
 require 'test_helper'
-
+require 'pry'
 class LearnTipTest < ActiveSupport::TestCase
   
   setup do 
-    @learn_tip = learn_tips(:learn_tip_1)
+    @learn_tip = learn_tips(:learn_tip_2)
     @new_learn_tip = LearnTip.new(name: 'test learn tip',
-                                     lesson_id: 1,
-                                     user_id: 1,
+                                     lesson: lessons(:lesson_1),
+                                     user: users(:test_user_michael),
                                      link: 'www.something.com',
                                      description:"asdfasdfasdfasdfasd.asdfasdfasdfasdfasdfasdfasdfasdfasdfasdf")
   end  
   
-
+ test 'should be valid' do 
+  assert @new_learn_tip.save 
+end 
  
+
   test "requires user" do 
     @new_learn_tip.user_id = nil 
     assert_not @new_learn_tip.save
@@ -25,7 +28,7 @@ class LearnTipTest < ActiveSupport::TestCase
 
   test 'requires link' do 
     @new_learn_tip.link = nil 
-    assert_not @new_learn_tip.valid?
+    assert_not @new_learn_tip.save
   end 
 
   test 'link does not exceed length' do 
