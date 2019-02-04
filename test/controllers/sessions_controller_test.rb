0b48c669edcc38606cef3c9ceac289b07b5dfae3
JerYoMat <jeremy.emata@gmail.com>
2019-02-03 21:37:24 -0500

    added session controller test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get new' do 
+    get login_path 
+    assert_response :success
+  end 
+
+  test 'after logging out user goes to root' do 
+    log_in_as(users(:test_user_michael))
+    delete logout_path 
+    assert_redirected_to root_path, 'users that log out should be redirected to home'
+  end 
 end
