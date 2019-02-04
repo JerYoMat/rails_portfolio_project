1ba606fce263505135ff6918fafef6a712432338
JerYoMat <jeremy.emata@gmail.com>
2019-02-04 16:12:29 -0500

    set test to run in order

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
-
+  config.active_support.test_order :sorted
   # Store uploaded files on the local file system in a temporary directory
   config.active_storage.service = :test
 
