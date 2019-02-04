3d6c524302dbe0a8dd13e743d40fa7ca836a31e8
JerYoMat <jeremy.emata@gmail.com>
2019-02-03 22:03:54 -0500

    added learn_tip validations

--- app/models/learn_tip.rb
+++ app/models/learn_tip.rb
@@ -2,5 +2,9 @@ class LearnTip < ApplicationRecord
 belongs_to :user 
 belongs_to :lesson 
 
+validates :link, presence: true, length: {maximum: 255}
+validates :name, presence: true, length: {maximum: 255}
+
+
 
 end
